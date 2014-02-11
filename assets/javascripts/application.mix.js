f2c28587add01a647ae62146ee1ee5ed44e80849
Ali Servet Donmez <asd@pittle.org>
2014-02-11 16:51:30 +0100

    Initial contact page layout

---
+++ assets/javascripts/application.mix.js
@@ -0,0 +1,8 @@
+# ==============================================================================
+# ADD YOUR CUSTOM SCRIPTS AFTER THE "MARKER LINE" BELOW IN THIS FILE !!!
+# ==============================================================================
+
+# Detect HTML5 and CSS3 features in the user’s browser.
+@import "modernizr/modernizr";
+
+# MARKER LINE --> //////////////////////////////////////////////////////////////
