a36191e81dda6730a6366774797ae6f475a16920
Ali Servet Donmez <asd@pittle.org>
2014-02-25 17:27:53 +0100

    Clean slate for the new layout

--- assets/javascripts/application.mix.js
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# ADD YOUR CUSTOM SCRIPTS AFTER THE "MARKER LINE" BELOW IN THIS FILE !!!
-# ==============================================================================
-
-# Detect HTML5 and CSS3 features in the user’s browser.
-@import "modernizr/modernizr";
-
-# MARKER LINE --> //////////////////////////////////////////////////////////////
